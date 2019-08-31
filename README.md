a5204bc919dbdb3aa5c2fac9d177766b54c80469
Upadhyay <prayag.upd@gmail.com>
2019-08-31 11:32:02 -0700

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 
 ```bash
 gradle clean jar
-java -jar build/libs/cache-1.0-SNAPSHOT.jar localhost 11211 add session1 900 updupdupd
-java -jar build/libs/cache-1.0-SNAPSHOT.jar localhost 11211 get session1
 ```
+
+set cache
+---------
+
+```bash
+java -jar build/libs/cache-service-1.0-SNAPSHOT.jar localhost 11211 add user_session1 900 {"session_id":"d38b5370-0dda-400f-a620-5c95e1803076"}
+```
+
+get cache
+---
+
+```bash
+java -jar build/libs/cache-service-1.0-SNAPSHOT.jar localhost 11211 get user_session1
+connecting to : localhost:11211
+2019-08-31 11:27:03.034 INFO net.spy.memcached.MemcachedConnection:  Setting retryQueueSize to -1
+2019-08-31 11:27:03.059 INFO net.spy.memcached.MemcachedConnection:  Added {QA sa=localhost/127.0.0.1:11211, #Rops=0, #Wops=0, #iq=0, topRop=null, topWop=null, toWrite=0, interested=0} to connect queue
+====================
+operation get
+====================
+getting : user_session1
+get result: {session_id:
+2019-08-31 11:27:03.089 INFO net.spy.memcached.MemcachedConnection:  Shut down memcached client
+```
+
+update
+------
+
+touch
+------
+
+delete
+-------
+
+flush
+-----
--- src/main/java/com/cache/CacheApplication.java
+++ src/main/java/com/cache/CacheApplication.java
@@ -3,14 +3,14 @@ package com.cache;
 import net.spy.memcached.MemcachedClient;
 import net.spy.memcached.internal.OperationFuture;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class CacheApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String host = args[0];
         String port = args[1];
+
         System.out.println("connecting to : " + host + ":" + port);
 
         try {
@@ -28,20 +28,28 @@ public class CacheApplication {
             if (operation.equals("add")) {
                 String expiration = args[4];
                 String value = args[5];
-                System.out.println("adding key: " + key + ", value: " + value + ", expiration: " + expiration);
-                OperationFuture<Boolean> add = client.add(key, Integer.valueOf(expiration), value);
-                add.addListener($ -> {
-                    System.out.println("add result: " + $.get());
-                    client.shutdown();
-                });
+                addCache(client, key, expiration, value);
             } else if (operation.equals("get")) {
-                System.out.println("getting : " + key);
-                Object o = client.get(key);
-                System.out.println("get result: " + o);
+                getCache(client, key);
                 client.shutdown();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void getCache(MemcachedClient client, String key) {
+        System.out.println("getting : " + key);
+        Object o = client.get(key);
+        System.out.println("get result: " + o);
+    }
+
+    private static void addCache(MemcachedClient client, String key, String expiration, String value) {
+        System.out.println("adding key: " + key + ", value: " + value + ", expiration: " + expiration);
+        OperationFuture<Boolean> add = client.add(key, Integer.valueOf(expiration), value);
+        add.addListener($ -> {
+            System.out.println("add result: " + $.get());
+            client.shutdown();
+        });
     }
 }
